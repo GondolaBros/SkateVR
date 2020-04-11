847621b50985e7bc2c59aa91832e22a18e6fb567
Kirstin Corbett <kirstincorbett@Kirstins-MacBook-Pro.local>
2020-04-11 18:01:32 -0400

    Adding initial project files

---
+++ Source/SkateVR/SkateVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKATEVR_API ASkateVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
